175b7eca6639d5f884447529b4694fc7975ea5de
Siddarth Krishnan <siddarthkrishnan.99@live.com>
2019-11-29 19:08:13 -0500

    removed venv (just make it locally)

--- project_flask/app/routes.py
+++ project_flask/app/routes.py
@@ -3,5 +3,6 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-	#return something (e.g.,...)
 	return "Hello, World!"
+	#return something (e.g.,...)
+	#return render_template('index.html')
