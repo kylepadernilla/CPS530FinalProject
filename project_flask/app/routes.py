163f9c7b91700c024fc133b288e2b7b7a3eff9ee
Siddarth Krishnan <siddarthkrishnan.99@live.com>
2019-11-29 19:40:21 -0500

    Update routes.py

--- project_flask/app/routes.py
+++ project_flask/app/routes.py
@@ -4,6 +4,4 @@ from flask import render_template
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello, World!"
-	#return something (e.g.,...)
-	#return render_template('index.html')
+	return render_template('index.html')
